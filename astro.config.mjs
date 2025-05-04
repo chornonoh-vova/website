efcba83179201cce0029cc80fec8044cc80fb32d
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2025-05-04 20:56:41 +0300

    configure mdx, add first blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,26 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
+
+  integrations: [
+    mdx({
+      optimize: true,
+    }),
+  ],
 });
--- src/content/blog/test-post-1/index.md
+++
@@ -1,9 +0,0 @@
----
-title: "Test Post 1"
-description: "Test Post 1"
-date: 2025-05-01
----
-
-# Test Post 1
-
-This is the first test post
