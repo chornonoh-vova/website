48913147607fa492fb221114b681fb53681b3006
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2025-06-11 14:54:26 +0300

    Add scroll to top button for articles
    Refactor out the arrow button and used it in post card and new button
    Update code highlight themes for better accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,8 +18,8 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       themes: {
-        light: "github-light",
-        dark: "github-dark",
+        light: "github-light-default",
+        dark: "github-dark-default",
       },
     },
   },
